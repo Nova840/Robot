cf0373e00f70ab005988f8740fb31c8b3e9fa117
Nova840 <adamzmudzinski2@gmail.com>
2023-05-29 02:27:27 -0400

    Refactor keyboard_mouse_test.py

--- Stuff/keyboard_mouse_test.py
+++ Stuff/keyboard_mouse_test.py
@@ -1,4 +1,5 @@
 from pynput import keyboard, mouse
+from time import sleep
 
 def on_press(key):
     keyStr = str(key).upper()
@@ -8,33 +9,21 @@ def on_press(key):
     elif keyStr == "KEY.ESC":
         print("Escape is used to terminate the program, so you're not allowed to use escape as input.")
 
-def on_release(key):
-    return
-
-def k_listen():
-    with keyboard.Listener(on_press = on_press, on_release = on_release) as k_listener:
-        k_listener.join()
-
 def on_click(x, y, button, pressed):
-    if not pressed:
-        return
-    print("\nPressed: " + str(button))
-
-def m_listen():
-    with mouse.Listener(on_click = on_click) as m_listener:
-        m_listener.join()
+    if pressed:
+        print("\nPressed: " + str(button))
 
 def main():
     print("Press any key to see value. Press CTRL + C to exit.")
 
-    k_listener  = keyboard.Listener(on_press = on_press, on_release = on_release, suppress = False)
+    k_listener  = keyboard.Listener(on_press = on_press, suppress = False)
     k_listener.start()
 
     m_listener = mouse.Listener(on_click = on_click, suppress = False)
     m_listener.start()
 
     while True:
-        pass
+        sleep(1)
 
 if __name__ == "__main__":
     main()
